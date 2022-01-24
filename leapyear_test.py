394cd517574767af0bf059384ee4ccbed52e2510
plumest <plumest.chayathon@gmail.com>
2022-01-24 16:33:03 +0700

    Test year equal to 5

--- leapyear_test.py
+++ leapyear_test.py
@@ -2,9 +2,9 @@ import pytest
 
 
 def leapyear(number: int) -> bool:
-    if number == 1:
-        return False
-    return True
+    if number == 4:
+        return True
+    return False
 
 
 class TestLeapYearClass:
@@ -13,3 +13,6 @@ class TestLeapYearClass:
 
     def test_leapyear_when_input_is_4(self):
         assert leapyear(4) == True
+
+    def test_leapyear_when_input_is_5(self):
+        assert leapyear(5) == False
