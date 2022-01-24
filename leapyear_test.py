dbf0f1cd9c6e21648de6b6f9844227335d960f3f
plumest <plumest.chayathon@gmail.com>
2022-01-24 16:36:35 +0700

    Test year equal to 100

--- leapyear_test.py
+++ leapyear_test.py
@@ -2,20 +2,26 @@ import pytest
 
 
 def leapyear(number: int) -> bool:
+    if number == 100:
+        return False
+
     if not (number % 4):
         return True
     return False
 
 
 class TestLeapYearClass:
-    def test_leapyear_when_input_is_1(self):
+    def test_is_not_leapyear_when_input_is_1(self):
         assert leapyear(1) == False
 
-    def test_leapyear_when_input_is_4(self):
+    def test_is_leapyear_when_input_is_4(self):
         assert leapyear(4) == True
 
-    def test_leapyear_when_input_is_5(self):
+    def test_is_not_leapyear_when_input_is_5(self):
         assert leapyear(5) == False
 
-    def test_leapyear_when_input_is_8(self):
+    def test_is_leapyear_when_input_is_8(self):
         assert leapyear(8) == True
+
+    def test_is_not_leapyear_when_input_is_100(self):
+        assert leapyear(100) == False
