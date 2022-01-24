4873d7f17d8c093c0f0a518e3b53327ee89984e7
plumest <plumest.chayathon@gmail.com>
2022-01-24 16:31:56 +0700

    Testing year equal to 1

--- leapyear_test.py
+++ leapyear_test.py
@@ -1,11 +1,15 @@
-import unittest
 import pytest
 
 
 def leapyear(number: int) -> bool:
+    if number == 1:
+        return False
     return True
 
 
 class TestLeapYearClass:
+    def test_leapyear_when_input_is_1(self):
+        assert leapyear(1) == False
+
     def test_leapyear_when_input_is_4(self):
         assert leapyear(4) == True
