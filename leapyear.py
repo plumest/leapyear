fa7a343391a62481e571b311a9d00652fc5adf0b
plumest <plumest.chayathon@gmail.com>
2022-01-24 17:04:30 +0700

    Testing condition improvement

--- leapyear.py
+++ leapyear.py
@@ -3,9 +3,8 @@ def leapyear(number: int) -> bool:
     if not (number % 400):
         return True
 
-    if not (number % 100):
-        return False
-
-    if not (number % 4):
+    elif not (number % 4) and (number % 100):
         return True
-    return False
+
+    else:
+        return False
